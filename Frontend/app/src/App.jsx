7a490da8e91936463c57bbb185518a672540a355
Gus <gusmaimoun@gmail.com>
2025-02-06 10:08:57 -0500

    Adding new components to the app + remove max-width from the root

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar from './Navbar'; // Importing Navbar component
-import { Gallery } from './components/Gallery';
-import Hero from './components/Hero';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Navbar"; // Importing Navbar component
+import { Gallery } from "./components/Gallery";
+import Hero from "./components/Hero";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -12,10 +13,13 @@ function App() {
       <Hero />
       <h1>Welcome to MAXX ENERGY</h1>
       <div className="card">
-        <p>Welcome to the MAXX ENERGY Portal. Please login to access more features.</p>
+        <p>
+          Welcome to the MAXX ENERGY Portal. Please login to access more
+          features.
+        </p>
       </div>
       <Gallery />
-
+      <Footer />
     </>
   );
 }
